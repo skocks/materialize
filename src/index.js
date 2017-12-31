7d53e9d785859f76361dc86e9e9362d6865456b6
Sebastian Kocks <sebastian.kocks@wesotion.com>
2017-12-31 16:15:31 +0100

    remove unneeded stuff

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./ts/autocomplete/autocomplete");
-require("./ts/buttons/buttons");
-require("./ts/parallax/parallax");
-require("./ts/sidenav/sidenav");
-require("./ts/slider/slider");
-require("./sass/materialize.scss");
